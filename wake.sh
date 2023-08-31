becb19972fe1d3ad5f9c9091f9c04e436179f478
Moarram <papwansam@gmail.com>
2023-08-31 14:07:25 -0700

    wait for any key

--- wake.sh
+++ wake.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# This script disables sleeping on lid close
+# This script disables sleeping on lid close and re-enables it when quit
 SUDO=""
-(( $EUID > 0 )) && SUDO="sudo -n"
+(( $EUID > 0 )) && SUDO="sudo -n" # set $SUDO if we aren't running as root
 function finish {
-    $SUDO pmset disablesleep 0
-    printf "\nSleep has been re-enabled"
+    $SUDO pmset disablesleep 0 # enable sleep
+    printf "Sleep \e[32menabled\e[0m"
 }
-$SUDO pmset disablesleep 1 || exit
-trap finish EXIT
-printf "Sleep disabled... quit script to re-enable (Ctrl+C)\n"
-while :
-	do sleep 1000
-done
+$SUDO pmset disablesleep 1 || exit # disable sleep, exit if failed
+trap finish EXIT # run function when script exits
+printf "Sleep \e[31mdisabled\e[0m\n\e[2mPress any key to re-enable...\e[0m\n"
+read -rsn1 # wait for keypress
